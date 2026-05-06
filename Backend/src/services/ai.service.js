d732661f4621427deabd799499dd5c57245451bc
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-05-06 14:00:23 +0530

    fix: gemini model changed

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -203,7 +203,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
     `;
 
   const response = await ai.models.generateContent({
-    model: "gemini-3-flash-preview",
+    model: "gemini-2.5-flash-lite",
     contents: prompt,
     config: {
       responseMimeType: "application/json",
