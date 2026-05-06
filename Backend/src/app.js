dec67a481cf743a6d0f4290c28cb0104e7cd1807
Rahul Goyal <rahulgoyal83789@gmail.com>
2026-05-06 13:11:07 +0530

    cores issue fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const cors = require('cors');
+const app = express();
 
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin: [
         'http://localhost:5173',                    // local dev
         'https://braindrift.vercel.app',            // your vercel URL
     ],
-    credentials: true,
-}));
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
     credentials: true,
 }));
 
